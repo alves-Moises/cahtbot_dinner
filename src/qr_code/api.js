f86cc5be9344808a7236218b293a1fe252f6f573
alves-Moises <moisesinho_ra@hotmail.com>
2023-07-30 14:54:52 -0300

    delete: qrcode

--- src/qr_code/api.js
+++
@@ -1,8 +0,0 @@
-const mp = new MercadoPago('YOUR_PUBLIC_KEY');
-
-
-var mercadopago = require('mercadopago');
-mercadopago.configurations.setAccessToken(config.access_token);
-
-var response = await mercadopago.payment_methods.listAll();
-var payment_methods = response.body;

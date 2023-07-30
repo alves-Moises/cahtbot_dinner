e25f88864f5f9866b19da866bd1af854c82dc459
alves-Moises <moisesinho_ra@hotmail.com>
2023-07-30 19:10:26 -0300

    update: ControllerUser

--- src/functions/class/Controller.js/UserController.js
+++ src/functions/class/Controller.js/UserController.js
@@ -2,3 +2,18 @@
 User = require("../user/")
 
 users = {}
+
+const NewUser = (number) => {
+    users[number] = new User(number)
+}
+
+const AddItem = (number, order) => { // id, item, qtd, value
+    users[number].add_order(order)
+}
+
+
+
+module.exports = {
+    NewUser, 
+    AddItem
+}
